17057d47e2d63791750eca31f30a1675f707b46b
Yei-Linux <jesusalvan2010@hotmail.com>
2024-02-14 12:12:44 -0500

    refactor test helper

--- js/tests/helper.js
+++ js/tests/helper.js
@@ -1,6 +1,6 @@
 const assert = {
   strictEqual: (a, b) => expect(a).toBe(b),
-  isTrue: (a, b) => expect(a).toBe(b),
+  isTrue: (a, b) => expect(a).toBeTruthy(),
 };
 
 module.exports = { assert };
